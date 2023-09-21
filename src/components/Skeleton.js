897500582dd46333b6db5a36003f598315221eef
C10uld <nnn.notnow@gmail.com>
2023-09-21 11:33:24 +0900

    animation with TailwindCSS

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,6 +1,24 @@
 import classNames from "classnames";
 
-function Skeleton({ times }) {
+function Skeleton({ times, className }) {
+  const outerClassNames = classNames(
+    "relative",
+    "overflow-hidden",
+    "bg-gray-200",
+    "rountded",
+    "mb-2.5",
+    className
+  );
+  const innerClassNames = classNames(
+    "animate-shimmer",
+    "absolute",
+    "inset-0",
+    "-translate-x-full",
+    "bg-gradient-to-r",
+    "from-gray-200",
+    "via-white",
+    "to-gray-200"
+  );
   //     const boxes = [];
   //     for (let i = 0; i < times; i++) {
   //       boxes.push(<div key={i} />);
@@ -14,7 +32,11 @@ function Skeleton({ times }) {
     .fill(0) //새로 생성된 배열을 값 0으로 채운다.
     .map((_, i) => {
       //배열 요소를 매핑하고 <div> 요소의 새 배열 생성 map : 배열의 각 요소를 반복하는 데 사용한다.
-      return <div key={i} />;
+      return (
+        <div key={i} className={outerClassNames}>
+          <div className={innerClassNames} />
+        </div>
+      );
     });
 
   return boxes;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shimmer: {
+          "100%": { transform: "translateX(100%)" }, //요소를 오른쪽으로 100% 이동하겠다
+        },
+      },
+      animation: { shimmer: "shimmer 1.5s infinite" }, //해당 요소를 왼쪽 > 오른쪽 이동하고 반복하는데 1.5초 소요
+    },
   },
   plugins: [],
 };
