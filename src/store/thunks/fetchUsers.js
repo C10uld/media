1a9d3619a0accdad136886d25e86701e7e3639e4
C10uld <nnn.notnow@gmail.com>
2023-09-21 10:37:34 +0900

    add pause for test

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const reponse = await axios.get("http://localhost:3005/users");
 
+  //DEV ONLY - test 위함
+  await pause(1000);
+
   return reponse.data;
 });
+
+//DEV ONLY - test 위함
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
