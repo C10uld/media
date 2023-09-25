db74df0cb7307dc926cbf8695d875de4a615e12a
C10uld <nnn.notnow@gmail.com>
2023-09-25 11:56:34 +0900

    create endPoint

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,5 +1,26 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const albumsApi = createApi({
+  /* When we create an API, we get back a slice, somg thunks, action creators
+    and then a set of automatically generated hooks. */
   reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchAlbus: builder.query({
+        //for deciding what the name of a hook
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id, // ?userId = user.id //Key
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
 });
